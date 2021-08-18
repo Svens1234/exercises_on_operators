7fb4839e529aafb3aecd33de1782119f8c84abc7
Svenden123 <buhrus1@yandex.ru>
2021-08-18 14:29:01 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#st = 'String Print only the words that start with as in this sentence'
+#for word in st.split():
+    #if word[0].lower() == 's':
+        #print(word)
+
+
+#for num in range(0,11,2):
+    #print(num)
+
+
+
+
+
+#print([x for x in range(1,51) if x%3==0])
+
+
+
+
+
+#st ='Print every word in this sentence that has an even number of letters'
+#for word in st.split():
+    #if len(word) % 2 == 0:
+        #print(word + ' имеет чётную длину')
+
+
+
+
+#for num in range(1,101):
+    #if num%3 == 0 and num%5 == 0:
+        #print('FizzBuzz')
+    #elif num%3 == 0:
+        #print('Fizz')
+    #elif num%5 == 0:
+        #print('Buzz')
+    #else:
+        #print(num)
+
+
+
+st = 'Create a list of the first letters of every word in the list'
+print([word[0] for word in st.split()])
+
+
